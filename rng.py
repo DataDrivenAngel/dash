f8f920bf4aebd4a58d3340c69de17c9e27abaaed
YT <buildingwill@gmail.com>
2020-06-28 21:38:27 -0400

    update rng

--- rng.py
+++ rng.py
@@ -1,14 +1,25 @@
 from numpy.random import default_rng
 from scipy.stats import genlogistic
 import plotly.express as px
+import numpy as np
+
+###
 
 size = 500
+c = 0.4
+
+###
+
 rng = default_rng()
 
-x = genlogistic.stats(,)
+x = np.linspace(genlogistic.ppf(0.01, c),
+                genlogistic.ppf(0.99, c), size)
 y = 2*rng.random(size)
 z = 2*rng.random(size)
 
+###
 
-fix = px.scatter_3d(x = x, y=y, z=z)
+print (len(x),len(y), len(z))
+
+fix = px.scatter_3d(x = x, y = y, z = z)
 fix.show()
